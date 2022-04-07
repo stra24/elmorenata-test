244d44906ad7880cf74d28459fce2d99897a1e26
j.kato <juicekato@yahoo.co.jp>
2022-04-08 07:01:03 +0900

    SCSSのコンパイルでエラーになるので、修正。

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     'scss/at-mixin-argumentless-call-parentheses': 'always', // 互換性のため引数なしのmixin呼び出しの際は括弧強制
     'sh-waqar/declaration-use-variable': [['/color/', 'z-index', 'font-size']], // 変数使用を強制する。
     'color-function-notation': 'legacy', // SCSSは新仕様（rgb()、rgba()がスペース区切りになる）にまだ対応していないため、カンマ削除のフォーマットがされないようにする。
+    'alpha-value-notation': 'number', // これを指定しないとrgbaのアルファ値が％に変換されてSCSSのコンパイルがエラーになってしまう。
   },
 };
